f0299640fc02365c1b8e943c630139d06cdc57e5
anthonyb0824 <anthonybutler0824@gmail.com>
2022-04-15 20:07:21 -0500

    stub entity, configured hibernate

---
+++ JPAVideogames/src/test/java/com/skilldistillery/videogames/entities/VideogameTest.java
@@ -0,0 +1,48 @@
+package com.skilldistillery.videogames.entities;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class VideogameTest {
+	private static EntityManagerFactory emf;
+	private EntityManager em;
+	private Videogame game;
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+		emf = Persistence.createEntityManagerFactory("JPAVideogames");
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+		emf.close();
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+		em = emf.createEntityManager();
+		game = em.find(Videogame.class, 1);
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+		em.close();
+		game = null;
+	}
+
+	@Test
+	void test() {
+		assertNotNull(game);
+		assertEquals("name", game.getName());
+	}
+
+}
